52c887339549b3be467ea3eb91335fbe8d6b02b1
meszarostibor <meszarostibor74@outlook.hu>
2025-05-26 18:13:37 +0200

    Initial commit

---
+++ horgaszok.sql
@@ -0,0 +1,14 @@
+INSERT INTO `horgaszok` (`id`, `nev`, `eletkor`) VALUES
+(1, 'Kovács Béla', 35),
+(2, 'Nagy János', 42),
+(3, 'Szabó László', 29),
+(4, 'Tóth Ferenc', 50),
+(5, 'Molnár Péter', 37),
+(6, 'Farkas Attila', 44),
+(7, 'Varga Zoltán', 31),
+(8, 'Horváth István', 28),
+(9, 'Kiss Gábor', 39),
+(10, 'Balogh Sándor', 46),
+(11, 'Lakatos Róbert', 33),
+(12, 'Oláh Tamás', 41),
+(13, 'Szilágyi László', 27);
---
+++ tavak.sql
@@ -0,0 +1,4 @@
+INSERT INTO `tavak` (`id`, `nev`, `helyszin`) VALUES
+(1, 'Balaton', 'Magyarország'),
+(2, 'Velencei-tó', 'Magyarország'),
+(3, 'Tisza-tó', 'Magyarország');
